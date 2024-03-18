2e95b1188f70a113f5f3dd25d56a33b572d95038
Gordi <Gordi>
2024-03-18 09:37:50 +0200

    a

--- cmd.ps1
+++ cmd.ps1
@@ -40,5 +40,5 @@ if ($null -eq $ultimateScheme) {
 	# Import Ultimate Performance
 	powercfg -import "$env:temp\Ultimate Performance.pow" e9a42b02-d5df-448d-aa00-03f14749eb61
 	# Apply Ultimate Performance
-	powercfg /S $powerSchemeGuid
 }
+powercfg /S $powerSchemeGuid
